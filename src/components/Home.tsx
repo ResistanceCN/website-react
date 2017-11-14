bef3d0f6dfce452d5061a891063786afbc9e3e6d
axionl <axionl@aosc.io>
2017-11-14 23:52:25 +0800

    change max-width, add example articles

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -30,11 +30,14 @@ export default class Home extends React.Component {
         for (let i = 0; i < 10; ++i) {
             articles.push({
                 id: i,
-                title: 'test article - ' + i,
+                title: '宇囚 - ' + i,
                 author: 1,
-                tag: ['test', 'test 2'],
+                tag: ['科幻', '短片小说'],
                 date: new Date(),
-                content: 'test content'
+                content: '第一个开拓者在启航后85年返回，打通了连接太阳系与织女星系的虫洞，\
+                由此掀开了人类文明殖民银河系的大幕。资源由各个星系源源不断的流入人类手中，\
+                技术随着时间推移变得愈发出神入化，智慧的足迹踏遍银河系的颗行星，\
+                冒险家的故事传颂在整个星河。'
             });
         }
 
