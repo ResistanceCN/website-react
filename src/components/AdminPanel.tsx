92c418194340118c6d6ac4961c0be60b1ae8017b
Balthild <ibalthild@gmail.com>
2017-12-02 01:36:21 +0800

    Fix layouts

--- src/components/AdminPanel.tsx
+++ src/components/AdminPanel.tsx
@@ -1,63 +1,61 @@
 import './AdminPanel.scss';
 import React from 'react';
-import { Layout, Menu, Breadcrumb, Icon, Table, Card } from 'antd';
+import { Card, Icon, Layout, Menu, Table } from 'antd';
+
+const { Sider } = Layout;
+const { SubMenu } = Menu;
+const { Column } = Table;
 
 interface AdminToolsProps {
     icon: string;
     record: string;
 }
 
-const { SubMenu } = Menu;
+interface DemoRecord {
+    key: string;
+    name: string;
+    age: number;
+    address: string;
+}
 
-const columns = [{
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name'
-}, {
-    title: 'Age',
-    dataIndex: 'age',
-    key: 'age'
-}, {
-    title: 'Address',
-    dataIndex: 'address',
-    key: 'address'
-}, {
-    title: 'Action',
-    key: 'action'
-}];
-
-const data = [{
+const data: Array<DemoRecord> = [
+    {
         key: '1',
         name: 'John Brown',
         age: 32,
         address: 'New York No. 1 Lake Park'
-    }, {
+    },
+    {
         key: '2',
         name: 'Jim Green',
         age: 42,
         address: 'London No. 1 Lake Park'
-    }, {
-        key: '2',
-        name: 'Jim Green',
-        age: 42,
-        address: 'London No. 1 Lake Park'
-    }, {
-        key: '2',
-        name: 'Jim Green',
-        age: 42,
-        address: 'London No. 1 Lake Park'
-    }, {
+    },
+    {
         key: '3',
+        name: 'Jim Green',
+        age: 42,
+        address: 'London No. 1 Lake Park'
+    },
+    {
+        key: '4',
+        name: 'Jim Green',
+        age: 42,
+        address: 'London No. 1 Lake Park'
+    },
+    {
+        key: '5',
         name: 'Joe Black',
         age: 32,
         address: 'Sidney No. 1 Lake Park'
-}];
+    }
+];
 
 export default class AdminPanel extends React.Component {
     render() {
         return (
-            <div className="flex-spacer">
-                <div className="sidebar">
+            <Layout className="flex-spacer panel-layout">
+                <Sider>
                     <Menu
                         mode="inline"
                         defaultSelectedKeys={['1']}
@@ -83,12 +81,31 @@ export default class AdminPanel extends React.Component {
                             <Menu.Item key="12">option12</Menu.Item>
                         </SubMenu>
                     </Menu>
-                </div>
-                <div className="container">
-                    <Table columns={columns} dataSource={data} />
+                </Sider>
+                <div className="flex-spacer container-fluid panel-container">
+                    <Table dataSource={data}>
+                        <Column title="Name" dataIndex="name" key="name" />
+                        <Column title="Age" dataIndex="age" key="age" />
+                        <Column title="Address" dataIndex="address" key="address" />
+                        <Column
+                            title="Action"
+                            key="action"
+                            render={(text, record: DemoRecord) => (
+                                <span>
+                                    <a href="#">Action - {record.name}</a>
+                                    <span className="ant-divider" />
+                                    <a href="#">Delete</a>
+                                    <span className="ant-divider" />
+                                    <a href="#" className="ant-dropdown-link">
+                                    More actions <Icon type="down" />
+                                    </a>
+                                </span>
+                            )}
+                        />
+                    </Table>
                     <Card className="chart">Some data analysis charts</Card>
                 </div>
-            </div>
+            </Layout>
         );
     }
 }
