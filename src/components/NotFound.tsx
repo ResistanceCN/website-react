d41cab6cf997af91264b633fd16b5f91ad93b967
axionl <axionl@aosc.io>
2018-07-17 09:49:16 +0800

    [Update] NotFound Page: optimized svg file.

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -3,7 +3,7 @@ import React, { StatelessComponent } from 'react';
 const NotFound: StatelessComponent<{}> = props => (
     <div className="center-container flex-spacer">
         <img
-            src="/assets/img/salted-fish.svg"
+            src="/assets/img/salted-fish-min.svg"
             alt="404"
             style={{
                 maxWidth: '50%',
