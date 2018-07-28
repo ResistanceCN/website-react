3307f54c7cd3dff3fcfcd7315de1731dd31b1e1b
axionl <axionl@aosc.io>
2018-07-28 15:48:27 +0800

    Add TODO list for profile sidebar

--- src/components/ProfileSidebar.tsx
+++ src/components/ProfileSidebar.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import { Card, Timeline } from 'antd';
 
-function getTimeline(): Array<string> {
-    return [
-        'Create a services site 2015-09-01',
-        'Solve initial network problems 2015-09-01',
-        'Technical testing 2015-09-01',
-        'Network problems being solved 2015-09-01'
-    ];
-}
-
 export default (props: {}) => (
     <Card title="账户设置" bordered={false} className="content-menu">
-        // Demo
+        @TODO:
+        更换 Google 账户
+        绑定 Telegram
+        删除帐号
     </Card>
 );
